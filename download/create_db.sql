9ff63097d66843d940e8cab141fcd4932e4637b9
risapav <risapav@gmail.com>
2018-01-13 13:28:51 +0100

    Update create_db.sql

--- download/create_db.sql
+++ download/create_db.sql
@@ -1,11 +1,2 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  risap
- * Created: 13.1.2018
- */
-CREATE DATABASE IF NOT EXISTS `xaps`;
+CREATE DATABASE IF NOT EXISTS 'xaps';
 
