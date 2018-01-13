d2b0639aed06578307825959e5573ff4f00d7904
Pavol Risa <risapav@gmail.com>
2018-01-13 11:06:31 +0100

    Initial version

--- install_freeacs.sh
+++ install_freeacs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash 
 
 clear # clear terminal window
 TRUE='1'
@@ -8,24 +8,17 @@ JDK="openjdk-8-jre-headless"
 
 #mysql variables
 MYSQLROOTPW=""	# root password
-ACSDBPW=""		# acs user password
+ACSDBPW=""	# acs user password
 OLDACSDBPW=""	# old acs user password
 
 #################################
 #only root is allowed to change system settings
 function are_you_root {
-    echo "Prepare system for installation"
-    read -p "Do you run this script with sudo (root) permission? (y/n) " yn
-    case $yn in
-      [Yy]* ) echo "" ;;
-      *     ) echo "Installation must be run with root permission."
-              exit;;
-    esac
 	
-	if (( $EUID != 0 )); then
-		echo "Please run as root"
-		exit
-	fi
+    if (( $EUID != 0 )); then
+        echo "Installation must be run with root permission."
+        exit
+    fi
 	
     mkdir tmp
 }
@@ -61,7 +54,7 @@ function prepare_ports {
 #load prepared web app files from repository
 function download_resources {  
 
-    FILES=( Fusion_Installation.pdf core.war install2013R1.sql monitor.war shell.jar spp.war stun.war syslog.war tr069.war web.war ws.war tables.zip )
+    FILES=( Fusion_Installation.pdf core.war install_db.sql monitor.war shell.jar spp.war stun.war syslog.war tr069.war web.war ws.war tables.zip )
 
     for i in "${FILES[@]}" 
     do
@@ -74,76 +67,35 @@ function download_resources {
 #################################
 # add xaps user into mysql
 function create_freeacsdbuser {
-    freeacsdbuserok=`mysql -uroot -p$MYSQLROOTPW -e "SELECT count(user) FROM mysql.user where user = 'xaps'" 2> /dev/null | tail -n1`
-echo "pozor1 $freeacsdbuserok"
-    if [ "$freeacsdbuserok" != '2' ] ; then
-        mysql -uroot -p$MYSQLROOTPW -e "CREATE DATABASE xaps" 2> /dev/null
-        mysql -uroot -p$MYSQLROOTPW xaps -e "GRANT ALL ON xaps.* TO 'xaps' IDENTIFIED BY '$ACSDBPW'"  2> tmp/.tmp
-        mysql -uroot -p$MYSQLROOTPW xaps -e "GRANT ALL ON xaps.* TO 'xaps'@'localhost' IDENTIFIED BY '$ACSDBPW'" 2>> tmp/.tmp
-        freeacsdbuserok=`mysql -uroot -p$MYSQLROOTPW -e "SELECT count(user) FROM mysql.user where user = 'xaps'" 2> /dev/null | tail -n1`
-echo "pozor2 $freeacsdbuserok"
-        if [ "$freeacsdbuserok" != '2' ] ; then
-            echo "The FreeACS MySQL database users 'xaps' and 'xaps'@'localhost' is not found"
-            echo "in the mysql.user table. Maybe you stated the wrong MySQL root password??"
-            echo "Please make sure this is corrected, either by running this script again with"
-            echo "the correct root password or by running the equivalent of the following"
-            echo " SQL-statements:"
-            echo ""
-            echo "Running as MySQL Root user:"
-            echo "  CREATE DATABASE xaps"
-            echo "  GRANT ALL ON xaps.* TO 'xaps' IDENTIFIED BY 'A_PASSWORD'"
-            echo "  GRANT ALL ON xaps.* TO 'xaps'@'localhost' IDENTIFIED BY 'A_PASSWORD'"
-            echo ""
-            echo "Below are stderr output from the commands above - they may indicate"
-            echo "the problem at hand:"
-            echo "------------------------------------------------"
-            cat tmp/.tmp
-            echo "------------------------------------------------"
-            echo ""
-            exit
-        else
-            echo ""
-            echo "The FreeACS MySQL database user is OK. "
-            echo ""
-        fi
-    else
-        echo ""
-        echo "The FreeACS MySQL database user is OK. "
-        echo ""
-    fi
+    echo "create_freeacsdbuser start----------------------------------------------------------------"
+    freeacsdbuserok=`mysql --defaults-extra-file=root.pw -e "SELECT count(user) FROM mysql.user where user = 'xaps'" #2> /dev/null | tail -n1`
+    printf "CREATE DATABASE \`xaps\`;\n" > mkdb.sql
+    printf "GRANT ALL ON xaps.* TO \`xaps\` IDENTIFIED BY '%s';\n" "$ACSDBPW" >> mkdb.sql
+    printf "GRANT ALL ON xaps.* TO \`xaps\`@\`localhost\` IDENTIFIED BY '%s';\n" "$ACSDBPW" >> mkdb.sql
+    mysql --defaults-extra-file=root.pw -e mkdb.sql
+    echo "create_freeacsdbuser stop----------------------------------------------------------------"
 }
 #################################
 # transfer all tables into mysql DB
 function load_database_tables {
-    echo ""
-    echo "Loads all FreeACS table defintions into MySQL"
-    mysql -uxaps -p$ACSDBPW xaps < tmp/install2013R1.sql 2> tmp/.tmp
-    installtables=`wc -l tmp/.tmp | cut -b1-1`
-echo "pozor 3 $installtables"
-cat tmp/.tmp
-    if [ "$installtables" != '1' ] ; then
-        echo "The output from the installation of the tables indicate some"
-        echo "errors occurred:"
-        echo "------------------------------------------------"
-        cat tmp/.tmp
-        echo "------------------------------------------------"  
-        exit
-    else
-        echo "Loading of all FreeACS tables was OK"
-    fi
+    echo "load_database_tables stop----------------------------------------------------------------"
+    mysql --defaults-extra-file=user.pw -e install_bd.sql #2> .tmp
+    echo "load_database_tables stop----------------------------------------------------------------"
 }
 ###################################
 #setup mysql database to accept tomcat users
 function database_setup {
-
-    mkdir tmp/tables 2> /dev/null
-    unzip -o -q -d tmp/tables/ tmp/tables.zip
+    mkdir tables 2> /dev/null
+    unzip -o -q -d tables/ tables.zip
 
     verified='n'
     until [ $verified == 'y' ] || [ $verified == 'Y' ]; do
         read -p "State the root password for the MySQL database: " MYSQLROOTPW
         read -p "Is [$MYSQLROOTPW] correct? (y/n) " verified
     done
+
+    printf "[client]\nuser = %s\npassword = %s" "root" "$MYSQLROOTPW" > root.pw
+
     echo ""
     echo "Specify/create the password for the FreeACS MySQL user."
     echo "NB! The FreeACS MySQL user name defaults to 'xaps'"
@@ -157,29 +109,13 @@ function database_setup {
         read -p "Specify/create the password for the FreeACS MySQL user: " ACSDBPW
         read -p "Is [$ACSDBPW] correct? (y/n) " verified
     done
-    echo ""
-    create_freeacsdbuser
 
-    tablepresent=`mysql -uxaps -p$ACSDBPW xaps -e "SHOW TABLES LIKE 'unit_type'" 2> /dev/null  | wc -l`
-    if [ "$tablepresent" == "2" ] ; then
-        echo "WARNING! An important FreeACS table is found in the database,"
-            echo "indicating that the database tables have already been loaded. "
-            echo "If you decide to load all table definitions, you will delete "
-            echo "ALL FreeACS data in the database, and start over."
-        verified='n'
-        until [ $verified == 'y' ] || [ $verified == 'Y' ]; 
-        do
-            read -p "Load all table defintions (and overwriting all data)? (y/n): " yn
-            read -p "Is [$yn] correct? (y/n) " verified
-        done
-        echo 
-        if [ "$yn" == 'y' ] ; then
-            load_database_tables
-        fi
-    else
-        load_database_tables
-    fi
-    echo ""
+    printf "[client]\nuser = %s\npassword = %s" "xaps" "$ACSDBPW" > user.pw
+    
+    create_freeacsdbuser
+    load_database_tables
+    tablepresent=`mysql --defaults-extra-file=user.pw xaps -e "SHOW TABLES LIKE 'unit_type'" 2> /dev/null  | wc -l`
+
 }
 ###################################
 #configure tomcat server
@@ -192,12 +128,12 @@ function tomcat_setup {
 
     for i in "${ARCHIVES[@]}"
     do
-        unzip -j -q -o tmp/$i WEB-INF/classes/xaps*.properties > /dev/null 2>&1
-        zip -d -q tmp/$i WEB-INF/classes/xaps*.properties > /dev/null 2>&1
+        unzip -j -q -o $i WEB-INF/classes/xaps*.properties > /dev/null 2>&1
+        zip -d -q $i WEB-INF/classes/xaps*.properties > /dev/null 2>&1
     done
 
-    unzip -j -q -o tmp/shell.jar xaps-shell*.properties > /dev/null 2>&1
-    zip -d -q tmp/shell.jar xaps-shell*.properties > /dev/null 2>&1
+    unzip -j -q -o shell.jar xaps-shell*.properties > /dev/null 2>&1
+    zip -d -q shell.jar xaps-shell*.properties > /dev/null 2>&1
 
     # Changes the default FreeACS MySQL password in the property files
     OLDACSDBPW=`grep -e "^db.xaps.url" xaps-tr069.properties | cut -d" " -f3 | cut -b6-40 | cut -d"@" -f1`
@@ -220,12 +156,12 @@ function tomcat_setup {
         do
             overwrite=''
             if [ -f /var/lib/$TOMCAT/common/$propertyfile ] ; then
-                diff /var/lib/$TOMCAT/common/$propertyfile $propertyfile > tmp/.tmp
-                if [ -s tmp/.tmp ] ; then
+                diff /var/lib/$TOMCAT/common/$propertyfile $propertyfile > .tmp
+                if [ -s .tmp ] ; then
                     echo "  $propertyfile diff found, added diff to config-diff.txt - please inspect!"
                     echo "$propertyfile diff:" >> config-diff.txt
                     echo "------------------------------------------------" >> config-diff.txt
-                    cat tmp/.tmp >> config-diff.txt
+                    cat .tmp >> config-diff.txt
                     echo "" >> config-diff.txt
                 else 
                     mv -f $propertyfile /var/lib/$TOMCAT/common
@@ -240,12 +176,12 @@ function tomcat_setup {
     for propertyfile in "${PROPERTYFILES[@]}"
     do
         if [ -f /var/lib/$TOMCAT/shell/$propertyfile ] ; then
-            diff /var/lib/$TOMCAT/shell/$propertyfile $propertyfile > tmp/.tmp
-            if [ -s tmp/.tmp ] ; then
+            diff /var/lib/$TOMCAT/shell/$propertyfile $propertyfile > .tmp
+            if [ -s .tmp ] ; then
                 echo "  $propertyfile diff found, added diff to config-diff.txt - please inspect!"
                 echo "$propertyfile diff:" >> config-diff.txt
                 echo "------------------------------------------------" >> config-diff.txt
-                cat tmp/.tmp >> config-diff.txt
+                cat .tmp >> config-diff.txt
                 echo "" >> config-diff.txt
             else
                 mv -f $propertyfile /var/lib/$TOMCAT/shell
@@ -260,8 +196,8 @@ function tomcat_setup {
 
     # Copies all war, jar and property files into their correct location
     # This actually deploys the application into Tomcat  
-    mv tmp/*.war /var/lib/$TOMCAT/webapps
-    mv tmp/*.jar /var/lib/$TOMCAT/shell
+    mv *.war /var/lib/$TOMCAT/webapps
+    mv *.jar /var/lib/$TOMCAT/shell
     echo "All WAR/JAR/property files have been moved to Tomcat - servers have been deployed!"
   
     # Makes requests to http://hostname/ redirect to http://hostname/web
@@ -300,6 +236,7 @@ function shell_setup {
 ###################################
 #tidy up after completed configuration
 function cleanup {
+    cd ..
     rm -R tmp/
 }
 ###################################
@@ -310,8 +247,10 @@ install_apps
 prepare_ports
 
 download_resources
+cd tmp
 database_setup
 tomcat_setup
 shell_setup
+cd ..
 cleanup
 ###################################
